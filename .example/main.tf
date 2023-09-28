9355c7a0168f31a4b58b3358cabfda08dafd2086
Ari Laverty <11803028+arivictor@users.noreply.github.com>
2023-09-28 18:21:32 +1000

    add: escalation policy and schedule

--- .example/main.tf
+++ .example/main.tf
@@ -29,51 +29,49 @@ module "pagerduty" {
     }
   }
 
-  # schedules = {
-  #   "first_line" = {
-  #     name      = "First Line"
-  #     time_zone = "Europe/London"
-  #     teams = [
-  #       "terraformers"
-  #     ]
-  #     layers = {
-  #       "initial" = {
-  #         name                         = "Initial"
-  #         start                        = "2023-05-19T21:30:00Z"
-  #         rotation_virtual_start       = "2023-05-19T21:30:00Z"
-  #         rotation_turn_length_seconds = 604800
-  #         restriction = {
-  #           type              = "daily_restriction"
-  #           start_time_of_day = "08:00:00"
-  #           duration_seconds  = 32400
-  #         }
-  #         users = [
-  #           "john_smith"
-  #         ]
-  #       }
+  schedules = {
+    "first_line" = {
+      name      = "First Line"
+      time_zone = "Europe/London"
+      teams = [
+        "terraformers"
+      ]
+      layers = {
+        "initial" = {
+          name                         = "Initial"
+          start                        = "2023-05-19T21:30:00Z"
+          rotation_virtual_start       = "2023-05-19T21:30:00Z"
+          rotation_turn_length_seconds = 604800
+          restriction = {
+            type              = "daily_restriction"
+            start_time_of_day = "08:00:00"
+            duration_seconds  = 32400
+          }
+          users = [
+            "john_smith"
+          ]
+        }
 
-  #     }
-  #   }
+      }
+    }
+  }
 
-    
-  # }
-
-  # escalation_policies = {
-  #     "eng_esc_policy" = {
-  #       name      = "Engineering Escalation Policy"
-  #       num_loops = 2
-  #       teams     = ["terraformers"]
-  #       rules = [
-  #         {
-  #           escalation_delay_in_minutes = 10
-  #           targets = [
-  #             {
-  #               type     = "user_reference"
-  #               user_key = "john_smith"
-  #             }
-  #           ]
-  #         }
-  #       ]
-  #     }
-  #   }
+  escalation_policies = {
+      "eng_esc_policy" = {
+        name      = "Engineering Escalation Policy"
+        num_loops = 2
+        teams     = ["terraformers"]
+        rules = [
+          {
+            escalation_delay_in_minutes = 10
+            targets = [
+              {
+                type     = "user_reference"
+                user_key = "john_smith"
+              }
+            ]
+          }
+        ]
+      }
+    }
 }
--- variables.tf
+++ variables.tf
@@ -30,42 +30,42 @@ variable "teams" {
   default = {}
 }
 
-# variable "schedules" {
-#   description = "List of schedules to be created."
-#   type = map(object({
-#     name      = string
-#     time_zone = string
-#     teams     = list(string)
-#     # Todo: should be a list and dynamic?
-#     layers = map(object({
-#       name                         = string
-#       start                        = string
-#       rotation_virtual_start       = string
-#       rotation_turn_length_seconds = number
-#       users                        = list(string)
-#       restriction = object({
-#         type              = string
-#         start_time_of_day = string
-#         duration_seconds  = number
-#       })
-#     }))
-#   }))
-#   default = {}
-# }
+variable "schedules" {
+  description = "List of schedules to be created."
+  type = map(object({
+    name      = string
+    time_zone = string
+    teams     = list(string)
+    # Todo: should be a list and dynamic?
+    layers = map(object({
+      name                         = string
+      start                        = string
+      rotation_virtual_start       = string
+      rotation_turn_length_seconds = number
+      users                        = list(string)
+      restriction = object({
+        type              = string
+        start_time_of_day = string
+        duration_seconds  = number
+      })
+    }))
+  }))
+  default = {}
+}
 
-# variable "escalation_policies" {
-#   description = "List of escalation policies."
-#   type = map(object({
-#     name      = string
-#     num_loops = number
-#     teams     = list(string)
-#     rules = list(object({
-#       escalation_delay_in_minutes = number
-#       targets = list(object({
-#         type     = string
-#         user_key = string
-#       }))
-#     }))
-#   }))
-#   default = {}
-# }
+variable "escalation_policies" {
+  description = "List of escalation policies."
+  type = map(object({
+    name      = string
+    num_loops = number
+    teams     = list(string)
+    rules = list(object({
+      escalation_delay_in_minutes = number
+      targets = list(object({
+        type     = string
+        user_key = string
+      }))
+    }))
+  }))
+  default = {}
+}
